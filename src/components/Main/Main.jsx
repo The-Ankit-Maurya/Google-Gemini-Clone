97f427ffd2c83cb6a73cd247807061ba67def419
The-Ankit-Maurya <ankitmaurya20223@gmail.com>
2024-06-18 11:51:19 +0530

    Changed some code and added images in assets folder

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Main.css'
+
+const Main = () => {
+  return (
+    <div className='main'>
+        <div className="nav">
+            <p>Gemini</p>
+        </div>
+    </div>
+  )
+}
+
+export default Main
